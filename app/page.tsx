ac5b6fc1f8f3a316f8638fcb848b68a186ee1179
Kavehpa20 <kavepazhhohesh3@gmail.com>
2024-02-26 23:27:19 +0330

    working on header

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+"use client";
+
+import Navbar from "@/components/Navbar";
+
 export default function Home() {
-  return <main className="font-black text-3xl">hello next</main>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
