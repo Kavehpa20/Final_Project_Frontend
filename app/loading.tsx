4937dd94ccbbb1de61a6080ef85bb4ef94209316
Kavehpa20 <kavepazhhohesh3@gmail.com>
2024-03-03 15:39:12 +0330

    adding loading to category and product

---
+++ app/loading.tsx
@@ -0,0 +1,6 @@
+import LandingSkeleton from "@/components/skeleton/LandingSkeleton";
+
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return <LandingSkeleton />;
+}
