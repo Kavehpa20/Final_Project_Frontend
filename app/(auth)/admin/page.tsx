0a20bad0c8049475c77dbde35f70c3bf773c08e4
Kavehpa20 <kavepazhhohesh3@gmail.com>
2024-03-09 15:22:24 +0330

    Fixing Layouts

--- app/(auth)/admin/page.tsx
+++ app/(auth)/admin/page.tsx
@@ -1,9 +1,5 @@
 import AdminLoginForm from "@/components/forms/AdminLoginForm";
 
 export default function Login() {
-  return (
-    <>
-      <AdminLoginForm />
-    </>
-  );
+  return <AdminLoginForm />;
 }
